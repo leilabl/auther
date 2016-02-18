12e3774177dc88f0d334533185ad74adff630a8b
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-02-18 18:27:12 -0500

    integrating google Oauth - not finished

--- browser/app/main.js
+++ browser/app/main.js
@@ -4,5 +4,8 @@ var app = angular.module('auther', ['ui.router']);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
-	$urlRouterProvider.otherwise('/');
+  $urlRouterProvider.when('/auth/:provider', function() {
+    window.location.reload();
+  });
+  $urlRouterProvider.otherwise('/');
 });
--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,9 @@
 
 var app = require('express')();
 var path = require('path');
+var passport = require('passport');
+
+var User = require('../api/users/user.model.js')
 
 app.use(require('./session.middleware'));
 
@@ -10,6 +13,38 @@ app.use(require('./logging.middleware'));
 app.use(require('./requestState.middleware'));
 
 app.use(require('./statics.middleware'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+passport.use(
+  new GoogleStrategy({
+    clientID: '732973626706-svps5slge4dltegkhvlc71pd6ckutl0t.apps.googleusercontent.com',
+    clientSecret: 'ELDIwgga8xgW273IUQ7b2CUI',
+    callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
+  },
+  function(token, refreshToken, profile, done){
+    console.log('---', 'in verification callback', profile, '---');
+    User.create({
+      email: profile.emails[0].value,
+      google: {
+        id: profile.id,
+        token: token
+      }
+    }).then(function(user) {
+      done(user)
+    }).then(null, console.log)
+  })
+);
+
+app.get('/auth/google', passport.authenticate('google', { scope:'email' }))
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/'
+  })
+);
 
 app.use('/login', require('./login'));
 app.use('/signup', require('./signup'));
